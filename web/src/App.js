f3d734a1be89bd9e156c1ab93db805bcaa4b21a9
Kelvin Romero <kelvin.cordeiro@control.eng.br>
2020-01-21 16:29:30 -0300

    Starting frontend

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
+import './sidebar.css';
+import './main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/8683337?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Kelvin Romero</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde voluptas ea quos dolores fugiat ullam sunt, debitis corrupti commodi aspernatur. Natus, cumque labore animi veritatis rerum quis totam nam soluta.</p>
+            <a href="">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/8683337?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Kelvin Romero</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde voluptas ea quos dolores fugiat ullam sunt, debitis corrupti commodi aspernatur. Natus, cumque labore animi veritatis rerum quis totam nam soluta.</p>
+            <a href="">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/8683337?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Kelvin Romero</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde voluptas ea quos dolores fugiat ullam sunt, debitis corrupti commodi aspernatur. Natus, cumque labore animi veritatis rerum quis totam nam soluta.</p>
+            <a href="">Acessar perfil no github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/8683337?s=460&v=4" alt=""/>
+              <div className="user-info">
+                <strong>Kelvin Romero</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde voluptas ea quos dolores fugiat ullam sunt, debitis corrupti commodi aspernatur. Natus, cumque labore animi veritatis rerum quis totam nam soluta.</p>
+            <a href="">Acessar perfil no github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
