b13bdc55bbea21542ab0f9ebde302888f99b0fda
Kelvin Romero <kelvin.cordeiro@control.eng.br>
2020-01-22 16:04:01 -0300

    Adding Cors, Getting geolocation from browser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes.js');
+const cors = require('cors');
 const app = express();
 
 
@@ -20,6 +21,7 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-ey0ie.mongodb.net/w
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
